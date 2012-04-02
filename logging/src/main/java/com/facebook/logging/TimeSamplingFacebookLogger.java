e91263d4d88913194c7c633f8bd8df939ab51f87
Sam Rash <srash@fb.com>
2012-04-02 03:32:17 -0700

    [jcommon] restore fb-specific time-sampling logger
    Summary:
    mistakenly deleted what appeared to be duplicate classes; one was for log4j Logger, one was for com.facebook.logging.Logger
    
    restored
    
    Test Plan:
    mvn test
    
    Reviewers: nitayj, groys, wwang, ajaiswal, timw
    
    Reviewed By: nitayj
    
    CC: puma-dev@lists
    
    Differential Revision: https://phabricator.fb.com/D440360

--- logging/src/main/java/com/facebook/logging/TimeSamplingFacebookLogger.java
+++ logging/src/main/java/com/facebook/logging/TimeSamplingFacebookLogger.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * decorates a FacebookLogger with  the ability to sample logging at a specified window size 
+ * decorates a FacebookLogger with  the ability to sample logging at a specified window size
  * (1 per time specified)
  **/
 public class TimeSamplingFacebookLogger implements Logger {
