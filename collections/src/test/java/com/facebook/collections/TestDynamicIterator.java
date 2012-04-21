b5b097bed1713370d7cd3ba20c0df3bf713e6fc7
Sam Rash <srash@fb.com>
2012-04-21 09:58:02 -0700

    [jcommon] tighten up TestDynamicIterator
    Summary:
    1. add fast group
    2. factor out common iterator
    3. add test for no such element
    
    Test Plan: it's a unit test, it passes
    
    Reviewers: timw, groys, wwang
    
    Reviewed By: timw
    
    CC: puma-dev@lists, martint
    
    Differential Revision: https://phabricator.fb.com/D455125

--- collections/src/test/java/com/facebook/collections/TestDynamicIterator.java
+++ collections/src/test/java/com/facebook/collections/TestDynamicIterator.java
@@ -2,9 +2,11 @@ package com.facebook.collections;
 
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -20,9 +22,16 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class TestDynamicIterator {
-  @Test
+
+  private DynamicIterator<Integer> iterator;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    iterator = new DynamicIterator<Integer>(100);
+  }
+
+  @Test(groups = "fast")
   public void testAddSingle() throws InterruptedException {
-    DynamicIterator<Integer> iterator = new DynamicIterator<Integer>();
     iterator.add(1);
     iterator.finish();
 
@@ -32,9 +41,8 @@ public class TestDynamicIterator {
     assertFalse(iterator.hasNext());
   }
 
-  @Test
+  @Test(groups = "fast")
   public void testAddMultiple() throws InterruptedException {
-    DynamicIterator<Integer> iterator = new DynamicIterator<Integer>();
 
     List<Integer> expected = asList(1, 2, 3, 4, 5);
     for (Integer value : expected) {
@@ -50,10 +58,8 @@ public class TestDynamicIterator {
     assertFalse(iterator.hasNext());
   }
 
-  @Test
+  @Test(groups = "fast")
   public void testInterleaved() throws InterruptedException {
-    DynamicIterator<Integer> iterator = new DynamicIterator<Integer>(100);
-
     List<Integer> expected = asList(1, 2, 3, 4, 5);
     for (int value : expected) {
       iterator.add(value);
@@ -64,11 +70,9 @@ public class TestDynamicIterator {
     assertFalse(iterator.hasNext());
   }
 
-  @Test(expectedExceptions = IllegalStateException.class,
+  @Test(groups= "fast", expectedExceptions = IllegalStateException.class,
         expectedExceptionsMessageRegExp = ".*already finished.*")
   public void testDoesNotAllowAddsAfterFinish() throws InterruptedException {
-    DynamicIterator<Integer> iterator = new DynamicIterator<Integer>(100);
-
     iterator.add(1);
     iterator.add(2);
     iterator.finish();
@@ -76,10 +80,9 @@ public class TestDynamicIterator {
     iterator.add(3);
   }
 
-  @Test
+  @Test (groups = "fast")
   public void testMultipleWriters()
     throws ExecutionException, InterruptedException, TimeoutException {
-    final DynamicIterator<Integer> iterator = new DynamicIterator<Integer>(100);
 
     ExecutorService executor = Executors.newCachedThreadPool(
       new ThreadFactoryBuilder().setDaemon(true).build());
@@ -102,6 +105,14 @@ public class TestDynamicIterator {
 
     Set<Integer> result = consumer.get(1, TimeUnit.SECONDS);// failsafe in case there's a deadlock
     assertEquals(result, Sets.newHashSet(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+  }
+
+  @Test(groups = "fast", expectedExceptions = NoSuchElementException.class)
+  public void testNoSuchElement() throws Exception {
+    iterator.add(1);
+    iterator.finish();
+    iterator.next();
+    iterator.next();
   }
 
   private Callable<Void> producer(final DynamicIterator<Integer> iterator, final int... values)
