7b92e019a9ac4567ae44d0c680d9039820393bf9
Sam Rash <srash@fb.com>
2012-06-27 18:45:01 -0700

    [jcommon] Array and LongArray
    Summary:
    1. introduce a simplified "list" or in this case, Array interface.  It's generic so that it can be used in classes that want to be generic, but allows for primitive-backed implementations
    
    2. reference implementation of Array for Long.  There are data structures already implemented on long[] that if changed to use Array, can be changed to generic and then implementing Array for the type (one can use AbstractList possibly easier). Examples are the LongHashSet and a "heap" implementation for CTA that does 'flat' long-tuples.  Could be generified to any primtiive tuple
    
    I made this thread-safe, mostly by synchronizing the methods.  Even the iterator *should* be thread-safe, but definitely does not throw CME.  it makes a best-effort to give as much valid data as it can.  It will not throw CME or give invalid data.  It's contract is similar to ConcurrentHashMap in that you are seeing a series of values from the list that were in it at sometime.
    
    Ideal use of the iterator is NOT in a concurrent case, though, with some locking done to ensure this.
    
    Thought: we could provide a SynchronizedSimpleArray that worked same as Collections.synchronizedX since SimpleArray doesn't specify thread-safety.
    
    Test Plan:
    1. existing tests
    2. new unit tests for reference impl
    
    Reviewers: timw, dain, martint, nitayj
    
    Reviewed By: nitayj
    
    CC: jcommon@, dphillips
    
    Differential Revision: https://phabricator.fb.com/D506752

---
+++ collections/src/main/java/com/facebook/collections/LongArray.java
@@ -0,0 +1,269 @@
+package com.facebook.collections;
+
+import com.google.common.base.Preconditions;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * Reference implementation of SimpleArray using longs. Attempts to limit memory growth
+ * by allowing a small growth factor each resize
+ *
+ */
+@ThreadSafe
+public class LongArray implements Array<Long> {
+  private static final int DEFAULT_INITIAL_CAPACITY = 3;
+  // memory conservative growth amount here
+  private static final double MIN_GROWTH_FACTOR = 0.3;
+  private static final int MIN_GROWTH_AMOUNT = 8; // intel xeon cache line size is 64 bytes
+  private static final long EMPTY = -1;
+
+  private long[] data;
+  private volatile int nextWritePosition = 0;
+  private volatile int size = 0;
+
+  public LongArray(int initialCapacity) {
+    data = new long[initialCapacity];
+    Arrays.fill(data, EMPTY);
+  }
+
+  public LongArray() {
+    this(DEFAULT_INITIAL_CAPACITY);
+  }
+
+  @Override
+  public synchronized Long get(int i) throws IndexOutOfBoundsException {
+    if (i >= size()) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    // return null if the slot is empty
+    return data[i] >= 0 ? data[i] : null;
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public synchronized int capacity() {
+    return data.length;
+  }
+
+  @Override
+  public Long set(int i, Long value) throws ArrayIndexOutOfBoundsException {
+    if (value == null) {
+      throw new NullPointerException("null values not allowed");
+    }
+
+    synchronized (this) {
+      if (i >= data.length) {
+        throw new ArrayIndexOutOfBoundsException(
+          String.format("tried to set value at index %d, but  max index is %d", i, data.length - 1)
+        );
+      }
+
+      Long oldValue = data[i];
+
+      data[i] = value;
+
+      if (oldValue == EMPTY) {
+        size++;
+      }
+
+      return oldValue;
+    }
+  }
+
+  @Override
+  public synchronized int append(Long value) {
+    int myWritePosition;
+
+    if (nextWritePosition >= data.length) {
+      int sizeIncrease = (int) (data.length * MIN_GROWTH_FACTOR);
+
+      sizeIncrease = Math.max(sizeIncrease, MIN_GROWTH_AMOUNT);
+
+      internalResize(data.length + sizeIncrease);
+    }
+
+    // find an empty slot if the current one isn't (may have been set by set(int i, Long value)
+    while (!isEmpty(nextWritePosition)) {
+      nextWritePosition++;
+
+      if (nextWritePosition >= data.length - 1) {
+        // resize in advance for next write
+        resize(data.length + MIN_GROWTH_AMOUNT);
+      }
+    }
+
+    checkState(isEmpty(nextWritePosition) && nextWritePosition < data.length);
+
+    myWritePosition = nextWritePosition;
+    // prepare for next write
+    nextWritePosition++;
+    size++;
+    data[myWritePosition] = value;
+
+    return myWritePosition;
+  }
+
+  @Override
+  public synchronized Long remove(int i) throws ArrayIndexOutOfBoundsException {
+    if (isEmpty(i)) {
+      return null;
+    } else {
+      Long previousValue = data[i];
+
+      data[i] = EMPTY;
+      size--;
+
+      return convertValue(previousValue);
+    }
+  }
+
+  @Override
+  public int resize(int sizeHint) {
+    return internalResize(sizeHint);
+  }
+
+  /**
+   * @param sizeHint effectively the new size (NOT the increase). May be changed for memory or
+   *                 cache alignments as implementations see fit
+   * @return actual new size
+   */
+  private synchronized int internalResize(int sizeHint) {
+    Preconditions.checkArgument(sizeHint > 0, "sizeHint must be > 0");
+
+    // we can use the sizeHint directly. Cases we wouldn't would be to align the array on
+    // word boundaries and not waste space (ex: int arrays always even sized, etc. We could even
+    // use cache line boundaries to maximize the chance of cache write hits (believed 64 bytes
+    // on our Xeon L5520 processors "cache_alignment : 64")
+    int newSize = sizeHint;
+
+    long[] newData = new long[newSize];
+
+    System.arraycopy(data, 0, newData, 0, data.length);
+    Arrays.fill(newData, data.length, newData.length, EMPTY);
+    data = newData;
+
+    return newSize;
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  public Iterator<Long> iterator() {
+    return new Iter();
+  }
+
+  /**
+   * since the contract to clients is that null => empty slot, we convert our empty indicators
+   * to null here
+   *
+   * @param value input from data
+   * @return input if >= 0, else null
+   */
+  private Long convertValue(long value) {
+    return value >= 0 ? value : null;
+  }
+
+  private boolean isEmpty(int position) {
+    return data[position] < 0;
+  }
+
+  /**
+   * Thread safe iterator. Note: at the moment the iterator is created, sizeSnapshot elements exist
+   * in the array. It will terminate upon seeing the first sizeSnapshot elements, or when position
+   * is >= capacitySnapshot. This means fewer elements or different elements may be seen, but
+   * so goes the life of concurrent data structure access
+   * <p/>
+   * It also supports remove, but again, note that this may not have the intended effect if the
+   * underlying array is resized. Prefer directly removing an element with
+   * {@link #LongSimpleArray.remove(int i)}
+   */
+  private class Iter implements Iterator<Long> {
+    // index to iterate through the array.
+    private int position = 0;
+    // -2 is no read called yet, -1 means remove was calledon this position
+    private int lastReadPosition = -2;
+    private int numSeen = 0;
+    private long sizeSnapshot = size;
+    private int capacitySnapshot = data.length;
+    private Long nextValue = null;
+    // indicates if we've read the next value
+    private boolean readNextValue = false;
+    // indicates if last read attemp found a value
+    private boolean hasNextValue = false;
+
+    @Override
+    public boolean hasNext() {
+      synchronized (LongArray.this) {
+        if (readNextValue) {
+          return hasNextValue;
+        }
+
+        boolean retVal = numSeen < sizeSnapshot && position < capacitySnapshot;
+
+        // this loop finds the next non-null value, or due to concurrent changes, it may simply
+        // terminate if it iterates over the entire capacity of the array w/o finding sizeSnapshot
+        // elements
+        while (retVal) {
+          nextValue = convertValue(data[position]);
+
+          if (nextValue != null) {
+            break;
+          }
+
+          position++;
+          // since deletions could occur, hitting the end of capacity before the size is possible
+          retVal = numSeen < sizeSnapshot && position < capacitySnapshot;
+        }
+
+        if (retVal) {
+          lastReadPosition = position;
+        }
+
+        readNextValue = true;
+        hasNextValue = retVal;
+
+        return retVal;
+      }
+    }
+
+    @Override
+    public Long next() {
+      synchronized (LongArray.this) {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+
+        readNextValue = false;
+        position++;
+
+        return nextValue;
+      }
+    }
+
+    @Override
+    public void remove() {
+      synchronized (LongArray.this) {
+        if (lastReadPosition == -2) {
+          throw new IllegalStateException("next() has not been called yet");
+        }
+        if (lastReadPosition == -1) {
+          throw new IllegalStateException("remove already called for this position");
+        }
+
+        data[lastReadPosition] = EMPTY;
+        size--;
+        lastReadPosition = -1;
+      }
+    }
+  }
+}
---
+++ collections/src/test/java/com/facebook/collections/TestLongArray.java
@@ -0,0 +1,144 @@
+package com.facebook.collections;
+
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+
+public class TestLongArray {
+  private static final Logger LOG = Logger.getLogger(TestLongArray.class);
+
+
+  private LongArray arraySize4;
+  private LongArray arraySize16;
+  private LongArray array;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    array = new LongArray();
+    array.append(1L);
+    array.append(2L);
+    array.append(3L);
+    array.append(4L);
+    array.append(5L);
+    array.append(6L);
+    arraySize4 = new LongArray(4);
+    arraySize16 = new LongArray(16);
+  }
+
+  @Test(groups = "fast")
+  public void testSanity() throws Exception {
+    array.append(1L);
+    Assert.assertEquals(array.size(), 7);
+    Assert.assertEquals(array.get(0).longValue(), 1L);
+    array.append(2L);
+    Assert.assertEquals(array.size(), 8);
+    Assert.assertEquals(array.get(5).longValue(), 6L);
+    Assert.assertEquals(array.get(7).longValue(), 2L);
+  }
+
+  @Test(groups = "fast", expectedExceptions = ArrayIndexOutOfBoundsException.class)
+  public void testOutOfBounds() throws Exception {
+    Assert.assertEquals(array.size(), 6);
+    Assert.assertEquals(array.get(7).longValue(), 1L);
+  }
+
+  @Test(groups = "fast")
+  public void testResize() throws Exception {
+    Assert.assertEquals(arraySize4.size(), 0);
+    Assert.assertEquals(arraySize4.capacity(), 4);
+    arraySize4.append(0L);
+    arraySize4.append(1L);
+    arraySize4.append(2L);
+    arraySize4.append(4L);
+    arraySize4.append(8L);
+    Assert.assertEquals(arraySize4.size(), 5);
+    Assert.assertEquals(arraySize4.capacity(), 12);
+    Assert.assertEquals(arraySize4.get(0).longValue(), 0);
+    Assert.assertEquals(arraySize4.get(1).longValue(), 1);
+    Assert.assertEquals(arraySize4.get(2).longValue(), 2);
+    Assert.assertEquals(arraySize4.get(3).longValue(), 4);
+    Assert.assertEquals(arraySize4.get(4).longValue(), 8);
+  }
+
+  @Test(groups = "fast")
+  public void testSetNoSizeGrowth() throws Exception {
+    arraySize16.append(1L);
+    arraySize16.append(1L);
+    arraySize16.append(1L);
+    arraySize16.append(1L);
+    arraySize16.set(3, 10L);
+
+    Assert.assertEquals(arraySize16.size(), 4);
+    Assert.assertEquals(arraySize16.get(3).longValue(), 10L);
+  }
+
+  @Test(groups = "fast")
+  public void testSetSizeGrowth() throws Exception {
+    arraySize16.append(1L);
+    arraySize16.append(1L);
+    arraySize16.append(1L);
+    arraySize16.append(1L);
+    arraySize16.set(4, 10L);
+
+    Assert.assertEquals(arraySize16.size(), 5);
+    Assert.assertEquals(arraySize16.get(4).longValue(), 10L);
+  }
+
+  @Test(groups = "fast", expectedExceptions = ArrayIndexOutOfBoundsException.class)
+  public void testSetOutOfBounds() throws Exception {
+    array.set(array.capacity(), 100L);
+  }
+
+  @Test(groups = "fast")
+  public void testRemove() throws Exception {
+    array.remove(3);
+    Assert.assertNull(array.get(3));
+    Assert.assertEquals(array.size(), 5);
+    assertArrayIs(array, 1, 2, 3, 5, 6);
+  }
+
+  @Test(groups = "fast")
+  public void testIterator() throws Exception {
+    Iterator<Long> iter = array.iterator();
+    // hasNext() shouldn't be required
+    Assert.assertEquals(iter.next().longValue(), 1L);
+    // repeated hasNext() shouldn't change value
+    iter.hasNext();
+    iter.hasNext();
+    iter.hasNext();
+    Assert.assertEquals(iter.next().longValue(), 2L);
+    iter.remove();
+    Assert.assertEquals(iter.next().longValue(), 3L);
+    Assert.assertEquals(iter.next().longValue(), 4L);
+    Assert.assertEquals(iter.next().longValue(), 5L);
+    Assert.assertEquals(iter.next().longValue(), 6L);
+  }
+
+  @Test(groups = "fast", expectedExceptions = IllegalStateException.class)
+  public void testIteratorRemoveNoNext() throws Exception {
+    Iterator<Long> iter = array.iterator();
+  	iter.remove();
+  }
+
+  @Test(groups = "fast", expectedExceptions = IllegalStateException.class)
+  public void testIteratorRepeatedRemove() throws Exception {
+    Iterator<Long> iter = array.iterator();
+  	iter.next();
+    iter.remove();
+    Assert.assertEquals(iter.next().longValue(), 2L);
+    iter.remove();
+    iter.remove();
+  }
+
+  private void assertArrayIs(LongArray array, long... values) {
+    int i = 0;
+
+    for (Long arrayValue : array) {
+      Assert.assertEquals(arrayValue.longValue(), values[i]);
+      i++;
+    }
+  }
+}
