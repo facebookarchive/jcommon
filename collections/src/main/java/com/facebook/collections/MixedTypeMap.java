8e9bd7752f4bacd52c3a61bb4cb932861d1f2e15
Sam Rash <srash@fb.com>
2012-10-03 10:02:56 -0700

    [jcommon] with MixedTypeMap, switch to using guava TypeToken internally
    Summary:
    Using this allows reified generics such as List<String> as "types".
    The change is backwards compatible as far as the API
    
    Test Plan:
    1. existing unit tests
    2. added more tests reified cases and Class vs TypeToken apis
    
    Reviewers: timw, groys, dain
    
    Reviewed By: timw
    
    CC: puma-dev@lists, jcommon@, erling
    
    Differential Revision: https://phabricator.fb.com/D591205

--- collections/src/main/java/com/facebook/collections/MixedTypeMap.java
+++ collections/src/main/java/com/facebook/collections/MixedTypeMap.java
@@ -1,6 +1,8 @@
 package com.facebook.collections;
 
 
+import com.google.common.reflect.TypeToken;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,12 +23,25 @@ public class MixedTypeMap<K> {
     map.put(key, instance);
   }
 
+  public <V1, V2 extends V1> void put(K id, TypeToken<V1> clazz, V2 instance) {
+    Key<K, V1> key = Key.get(id, clazz);
+
+    map.put(key, instance);
+  }
+
   public <V1, V2 extends V1> void put(Key<K, V1> key, V2 instance) {
     map.put(key, instance);
   }
 
   public <V> V get(K id, Class<V> clazz) {
     Key<K, V> key = Key.get(id, clazz);
+
+    //noinspection unchecked
+    return (V) map.get(key);
+  }
+
+  public <V> V get(K id, TypeToken<V> type) {
+    Key<K, V> key = Key.get(id, type);
 
     //noinspection unchecked
     return (V) map.get(key);
