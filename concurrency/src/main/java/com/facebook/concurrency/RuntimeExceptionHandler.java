a7cd0e1937c349485421c58b8a904ada990a0931
Sam Rash <srash@fb.com>
2014-04-03 11:31:32 -0700

    make RuntimeExceptionHandler.INSTANCE frinal

--- concurrency/src/main/java/com/facebook/concurrency/RuntimeExceptionHandler.java
+++ concurrency/src/main/java/com/facebook/concurrency/RuntimeExceptionHandler.java
@@ -21,7 +21,7 @@ import com.facebook.util.exceptions.ExceptionHandler;
  * map any exception to a runtime exception
  */
 public class RuntimeExceptionHandler implements ExceptionHandler<RuntimeException> {
-  public static RuntimeExceptionHandler INSTANCE = new RuntimeExceptionHandler();
+  public static final RuntimeExceptionHandler INSTANCE = new RuntimeExceptionHandler();
 
   @Override
   public <S extends Exception> RuntimeException handle(S e) {
