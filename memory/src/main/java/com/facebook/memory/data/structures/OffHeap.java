bc55877b1f6e8e730b8e8beffa41a19615eb74c4
Sam Rash <srash@fb.com>
2015-08-12 09:55:56 -0700

    # On branch off-heap-storage-slots # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   memory/pom.xml #	new file:   memory/src/main/java/com/facebook/memory/MemoryConstants.java #	new file:   memory/src/main/java/com/facebook/memory/data/structures/OffHeap.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/DoublyLinkedListNode.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/LinkedListNode.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/ByteArraySlot.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/ByteArraySlotAccessor.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/FieldType.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/IntAccessor.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/IntSlot.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/LongAccessor.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/LongSlot.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/OffHeapStructure.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/PointerAccessor.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/PointerSlot.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/Slot.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/SlotAccessor.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/Struct.java #	new file:   memory/src/main/java/com/facebook/memory/data/types/definitions/Structs.java #	new file:   memory/src/test/java/com/facebook/memory/data/types/definitions/TestSlots.java #	modified:   pom.xml # # Untracked files not listed (use -u option to show untracked files)

---
+++ memory/src/main/java/com/facebook/memory/data/structures/OffHeap.java
@@ -0,0 +1,5 @@
+package com.facebook.memory.data.structures;
+
+public interface OffHeap {
+  long getAddress();
+}
---
+++ memory/src/main/java/com/facebook/memory/data/types/definitions/IntAccessor.java
@@ -0,0 +1,21 @@
+package com.facebook.memory.data.types.definitions;
+
+import sun.misc.Unsafe;
+
+import com.facebook.memory.UnsafeAccessor;
+
+public class IntAccessor extends SlotAccessor {
+  private static final Unsafe unsafe = UnsafeAccessor.get();
+
+  public IntAccessor(long baseAddress, Slot slot) {
+    super(baseAddress, slot);
+  }
+
+  public void put(int i) {
+    unsafe.putInt(getOffset(), i);
+  }
+
+  public int get() {
+    return unsafe.getInt(getOffset());
+  }
+}
