243e9e723c6ea755f9e196f82133a6214f0253f1
Tim Williamson <timw@fb.com>
2016-04-13 12:02:57 -0700

    Fix getTimeIntervalStart() handling of daylight saving gaps
    Replace `TimeIntervalType.clearField()` with `DateTimeFieldType.roundFloorCopy()`, since `clearField()` throws an exception if the time lies in a daylight-saving gap.
    
    Several timezones have a daylight saving cutover of midnight: 00:00 becomes 01:00. The old implementation of `TimeIntervalType` truncated the time interval by setting each `subType` field to its minimum value. Unfortunately, if this time happened to correspond to a DST gap, then an `IllegalFieldValueException` exception would be thrown, e.g.:
    
        org.joda.time.IllegalFieldValueException: Value 0 for hourOfDay is not supported: Illegal instant due to time zone offset transition (daylight savings time 'gap'): 2016-03-13T00:00:00.000 (America/Havana)

--- util/src/main/java/com/facebook/util/TimeIntervalType.java
+++ util/src/main/java/com/facebook/util/TimeIntervalType.java
@@ -25,14 +25,24 @@ import org.joda.time.PeriodType;
 import org.joda.time.chrono.ISOChronology;
 import org.joda.time.field.FieldUtils;
 
+import static org.joda.time.DateTimeFieldType.centuryOfEra;
+import static org.joda.time.DateTimeFieldType.dayOfMonth;
+import static org.joda.time.DateTimeFieldType.hourOfDay;
+import static org.joda.time.DateTimeFieldType.millisOfSecond;
+import static org.joda.time.DateTimeFieldType.minuteOfHour;
+import static org.joda.time.DateTimeFieldType.monthOfYear;
+import static org.joda.time.DateTimeFieldType.secondOfMinute;
+import static org.joda.time.DateTimeFieldType.weekOfWeekyear;
+import static org.joda.time.DateTimeFieldType.yearOfCentury;
+
 /**
  * Represents a time interval type when specifying a time period. Instances of
  * this class provide means to calculate the start instant of the interval
  * containing a given time instant.
- * 
+ *
  * <p>
- * Each interval type computes the time interval assuming that they start 
- * from it's minimum value. For example, MINUTE represents the time interval 
+ * Each interval type computes the time interval assuming that they start
+ * from it's minimum value. For example, MINUTE represents the time interval
  * starting from the 0th minute in the containing hour. So given a time instant
  * 2011-10-09T13:11:49, and a time interval type of MINUTE and length 3, the
  * start instant of the time interval containing that instant will be
@@ -40,31 +50,31 @@ import org.joda.time.field.FieldUtils;
  * </p>
  */
 public enum TimeIntervalType {
-  MILLIS(PeriodType.millis(), DateTimeFieldType.millisOfSecond(), null) {
+  MILLIS(PeriodType.millis(), millisOfSecond(), secondOfMinute()) {
     @Override
     public Period toPeriod(int length) {
       return Period.millis(length);
     }
   },
-  SECOND(PeriodType.seconds(), DateTimeFieldType.secondOfMinute(), MILLIS) {
+  SECOND(PeriodType.seconds(), secondOfMinute(), minuteOfHour()) {
     @Override
     public Period toPeriod(int length) {
       return Period.seconds(length);
     }
   },
-  MINUTE(PeriodType.minutes(), DateTimeFieldType.minuteOfHour(), SECOND) {
+  MINUTE(PeriodType.minutes(), minuteOfHour(), hourOfDay()) {
     @Override
     public Period toPeriod(int length) {
       return Period.minutes(length);
     }
   },
-  HOUR(PeriodType.hours(), DateTimeFieldType.hourOfDay(), MINUTE) {
+  HOUR(PeriodType.hours(), hourOfDay(), dayOfMonth()) {
     @Override
     public Period toPeriod(int length) {
       return Period.hours(length);
     }
   },
-  DAY(PeriodType.days(), DateTimeFieldType.dayOfMonth(), HOUR) {
+  DAY(PeriodType.days(), dayOfMonth(), monthOfYear()) {
     @Override
     public Period toPeriod(int length) {
       return Period.days(length);
@@ -76,19 +86,19 @@ public enum TimeIntervalType {
    * as a time interval type, until we can have the weeks starting on the
    * correct day (Sunday/Monday) per the locale.
    */
-  WEEK(PeriodType.weeks(), DateTimeFieldType.weekOfWeekyear(), DAY) {
+  WEEK(PeriodType.weeks(), weekOfWeekyear(), yearOfCentury()) {
     @Override
     public Period toPeriod(int length) {
       return Period.weeks(length);
     }
   },
-  MONTH(PeriodType.months(), DateTimeFieldType.monthOfYear(), DAY) {
+  MONTH(PeriodType.months(), monthOfYear(), yearOfCentury()) {
     @Override
     public Period toPeriod(int length) {
       return Period.months(length);
     }
   },
-  YEAR(PeriodType.years(), DateTimeFieldType.yearOfCentury(), MONTH) {
+  YEAR(PeriodType.years(), yearOfCentury(), centuryOfEra()) {
     @Override
     public Period toPeriod(int length) {
       return Period.years(length);
@@ -97,36 +107,28 @@ public enum TimeIntervalType {
 
   private final PeriodType periodType;
   private final DateTimeFieldType fieldType;
-  private final TimeIntervalType subType;
+  private final DateTimeFieldType truncateFieldType;
 
-  /**
-   * Creates an instance.
-   * 
-   * @param type The field type corresponding to this interval type.
-   * @param subType The subtype for this type.
-   */
-  private TimeIntervalType(
-    PeriodType periodType,
-    DateTimeFieldType type,
-    TimeIntervalType subType
+  TimeIntervalType(
+    PeriodType periodType, DateTimeFieldType fieldType, DateTimeFieldType truncateFieldType
   ) {
     this.periodType = periodType;
-    this.fieldType = type;
-    this.subType = subType;
+    this.fieldType = fieldType;
+    this.truncateFieldType = truncateFieldType;
   }
 
   /**
    * Returns a period representing this interval type.
-   * 
+   *
    * @param length the multiple of the base period unit
    * @return the period value
    */
   public abstract Period toPeriod(int length);
 
   /**
-   * Validates that the specified interval value is valid for this 
+   * Validates that the specified interval value is valid for this
    * interval type in the supplied time zone.
-   * 
+   *
    * @param timeZone the time zone
    * @param intervalLength the interval length
    */
@@ -135,59 +137,45 @@ public enum TimeIntervalType {
     if (intervalLength < 1
       || intervalLength > field.getMaximumValue()) {
       throw new IllegalArgumentException(
-        "Supplied value " + intervalLength
-          + " is out of bounds for " + name()
+        "Supplied value " + intervalLength + " is out of bounds for " + name()
       );
     }
   }
 
 
   /**
-   * Gets the start instant given the event instant, interval length 
+   * Gets the start instant given the event instant, interval length
    * and the time zone for this interval type.
-   * 
+   *
    * @param instant the event time instant.
    * @param length the interval length
-   * 
-   * @return the start instant of the interval of given length that contains 
-   * the supplied time instant in the supplied time zone 
+   *
+   * @return the start instant of the interval of given length that contains
+   * the supplied time instant in the supplied time zone
    */
-  public DateTime getTimeIntervalStart(
-    DateTime instant,
-    long length
-  ) {
+  public DateTime getTimeIntervalStart(DateTime instant, long length) {
     validateValue(instant.getZone(), length);
-    // Get the time in the specified timezone
-    DateTime periodStart = instant;
-    // Clear all the fields for this intervalType and its subtypes
-    TimeIntervalType timeIntervalType = this;
-    while (timeIntervalType != null) {
-      periodStart = timeIntervalType.clearField(periodStart);
-      timeIntervalType = timeIntervalType.subType;
-    }
+
+    // Reset all the fields
+    DateTime periodStart = instant.property(truncateFieldType)
+      .roundFloorCopy();
     // figure out the which time interval does the instant lie in
     Period period = new Period(periodStart, instant, periodType);
     DurationField durationField = fieldType.getField(instant.getChronology()).getDurationField();
     int diff = period.get(durationField.getType());
     long startDelta = (diff / length) * length;
+
     return periodStart.withFieldAdded(durationField.getType(), FieldUtils.safeToInt(startDelta));
   }
 
   /**
    * Returns the number of milliseconds per unit of this interval type.
-   * 
+   *
    * @return the number of milliseconds per unit of this interval type.
    */
   public long toDurationMillis() {
     return fieldType.getDurationType().getField(
       // Assume that durations are always in UTC
       ISOChronology.getInstance(DateTimeZone.UTC)).getUnitMillis();
-  }
-
-  protected DateTime clearField(DateTime value) {
-    return value.withField(
-      fieldType,
-      fieldType.getField(value.getChronology()).getMinimumValue()
-    );
   }
 }
